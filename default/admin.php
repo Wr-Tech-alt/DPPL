66c99a88045bd5817f77444d0e8a707289848501
RenRyoo <narendraaryo2005@gmail.com>
2025-06-21 14:53:24 +0000

    coy

--- default/admin.php
+++ default/admin.php
@@ -1,121 +1,124 @@
 <?php
-$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as proses  from tb_pengaduan where status='Proses'");
-while ($data = $sql->fetch_assoc()) {
-	$proses = $data['proses'];
-}
+// Pastikan koneksi dan session dimulai
+include '../inc/koneksi.php'; // Path disesuaikan karena berada di sub-folder
 
-$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as tanggapi  from tb_pengaduan where status='Tanggapi'");
-while ($data = $sql->fetch_assoc()) {
-	$tangan = $data['tanggapi'];
-}
-
-$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as selesai  from tb_pengaduan where status='Selesai'");
-while ($data = $sql->fetch_assoc()) {
-	$sel = $data['selesai'];
-}
-
-$sql = $koneksi->query("SELECT COUNT(id_pengadu) as orang  from tb_pengadu");
-while ($data = $sql->fetch_assoc()) {
-	$or = $data['orang'];
+// Periksa apakah pengguna sudah login dan apakah level-nya 'admin' atau 'petugas'
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true || ($_SESSION['level'] !== 'admin' && $_SESSION['level'] !== 'petugas')) {
+    header("Location: ../login.php"); // Arahkan kembali ke halaman login
+    exit();
 }
 ?>
-<hr>
-<div>
-	<center>
-		<h1>Selamat Datang,
-			<?= $data_nama ?> -
-			<?= $data_level ?>
-		</h1>
-	</center>
-</div>
-<hr>
-<div class="col-md-3 col-sm-6 col-xs-6">
-	<div class="panel panel-back noti-box">
-		<span class="icon-box bg-color-red set-icon">
-			<i class="fa fa-bars"></i>
-		</span>
-		<div class="text-box">
-			<h1>
-				<?= $proses; ?>
-			</h1>
-			<p>Pengaduan Menunggu</p>
-			<a href="?page=aduan_admin">Detail
-			</a>
-		</div>
-	</div>
-</div>
-<div class="col-md-3 col-sm-6 col-xs-6">
-	<div class="panel panel-back noti-box">
-		<span class="icon-box bg-color-green set-icon">
-			<i class="fa fa-bars"></i>
-		</span>
-		<div class="text-box">
-			<h1>
-				<?= $tangan; ?>
-			</h1>
-			<p>Pengaduan Ditanggapi</p>
-			<a href="?page=aduan_admin_tanggap">Detail
-			</a>
-		</div>
-	</div>
-</div>
-<div class="col-md-3 col-sm-6 col-xs-6">
-	<div class="panel panel-back noti-box">
-		<span class="icon-box bg-color-blue set-icon">
-			<i class="fa fa-bars"></i>
-		</span>
-		<div class="text-box">
-			<h1>
-				<?= $sel; ?>
-			</h1>
-			<p>Pengaduan Selesai</p>
-			<a href="?page=aduan_admin_selesai">Detail
-			</a>
-		</div>
-	</div>
-</div>
-<div class="col-md-3 col-sm-6 col-xs-6">
-	<div class="panel panel-back noti-box">
-		<span class="icon-box bg-color-green set-icon">
-			<i class="fa fa-users"></i>
-		</span>
-		<div class="text-box">
-			<h1>
-				<?= $or; ?>
-			</h1>
-			<p>Pengadu Data</p>
-			<a href="?page=pengadu_view">Detail
-			</a>
-		</div>
-	</div>
-</div>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<!-- Copyrights -->
-<footer>
-	<div class="bg-light py-4">
-		<div class="container text-center">
-			<a href="https://www.instagram.com/stmijakarta/" target="blank">© 2025 - Sicepu</a>
-		</div>
-	</div>
-</footer>
-<!-- End -->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard Admin</title>
+    <link href="../assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="../assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="../assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <link href="../assets/css/custom.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="admin.php">Admin Dashboard</a> 
+            </div>
+            <div style="color: white; padding: 15px 50px 5px 50px; float: right; font-size: 16px;"> 
+                Last access : <?php echo date("d-M-Y H:i"); ?> &nbsp; 
+                <a href="../logout.php" class="btn btn-danger square-btn-adjust">Logout</a> 
+            </div>
+        </nav>   
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                    <li class="text-center">
+                        <img src="../assets/img/find_user.png" class="user-image img-responsive"/>
+                    </li>
+                    <li><a class="active-menu"  href="admin.php"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a></li>
+                    <li><a  href="../admin/aduan/adu_tampil.php"><i class="fa fa-desktop fa-3x"></i> Kelola Aduan</a></li>
+                    <?php if ($_SESSION['level'] == 'admin'): // Hanya admin yang bisa kelola pengguna ?>
+                    <li><a  href="../admin/pengguna/pengguna_tampil.php"><i class="fa fa-users fa-3x"></i> Kelola Pengguna</a></li>
+                    <?php endif; ?>
+                    <li><a  href="../admin/jenis/jenis_tampil.php"><i class="fa fa-list fa-3x"></i> Kelola Jenis Pengaduan</a></li>
+                    <li><a  href="../admin/laporan/laporan.php"><i class="fa fa-file-text fa-3x"></i> Laporan</a></li>
+                    <li><a  href="../admin/telegram/telegram.php"><i class="fa fa-paper-plane fa-3x"></i> Notifikasi Telegram</a></li>
+                </ul>
+            </div>
+        </nav>  
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Admin Dashboard</h2>   
+                        <h5>Selamat datang, <?php echo htmlspecialchars($_SESSION['nama_lengkap']); ?> ! Anda login sebagai <?php echo htmlspecialchars($_SESSION['level']); ?>. </h5>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+                        <div class="panel panel-back noti-box">
+                            <span class="icon-box bg-color-red set-icon">
+                                <i class="fa fa-envelope-o"></i>
+                            </span>
+                            <div class="text-box" >
+                                <p class="main-text">120 Laporan</p>
+                                <p class="text-muted">Baru</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+                        <div class="panel panel-back noti-box">
+                            <span class="icon-box bg-color-green set-icon">
+                                <i class="fa fa-bars"></i>
+                            </span>
+                            <div class="text-box" >
+                                <p class="main-text">300 Laporan</p>
+                                <p class="text-muted">Total</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+                        <div class="panel panel-back noti-box">
+                            <span class="icon-box bg-color-blue set-icon">
+                                <i class="fa fa-bell-o"></i>
+                            </span>
+                            <div class="text-box" >
+                                <p class="main-text">240 Pemberitahuan</p>
+                                <p class="text-muted">Baru</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+                        <div class="panel panel-back noti-box">
+                            <span class="icon-box bg-color-brown set-icon">
+                                <i class="fa fa-users"></i>
+                            </span>
+                            <div class="text-box" >
+                                <p class="main-text">150 Pengguna</p>
+                                <p class="text-muted">Terdaftar</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                </div>
+             </div>
+         </div>
+     <script src="../assets/js/jquery-1.10.2.js"></script>
+      <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/jquery.metisMenu.js"></script>
+     <script src="../assets/js/morris/raphael-2.1.0.min.js"></script>
+     <script src="../assets/js/morris/morris.js"></script>
+      <script src="../assets/js/custom.js"></script>
+    
+   
+</body>
+</html>
--- default/pengadu.php
+++ default/pengadu.php
@@ -1,114 +1,108 @@
 <?php
-$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as proses  from tb_pengaduan where status='Proses' and author='$data_id'");
-while ($data = $sql->fetch_assoc()) {
-	$proses = $data['proses'];
-}
+// Pastikan koneksi dan session dimulai
+include '../inc/koneksi.php'; // Path disesuaikan karena berada di sub-folder
 
-$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as tanggapi  from tb_pengaduan where status='Tanggapi' and author='$data_id'");
-while ($data = $sql->fetch_assoc()) {
-	$tangan = $data['tanggapi'];
+// Periksa apakah pengguna sudah login dan apakah level-nya 'masyarakat'
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true || $_SESSION['level'] !== 'masyarakat') {
+    header("Location: ../login.php"); // Arahkan kembali ke halaman login
+    exit();
 }
-
-$sql = $koneksi->query("SELECT COUNT(id_pengaduan) as selesai  from tb_pengaduan where status='Selesai' and author='$data_id'");
-while ($data = $sql->fetch_assoc()) {
-	$sel = $data['selesai'];
-}
-
 ?>
-<hr>
-<div>
-	<center>
-		<h1>Selamat Datang,
-			<?= $data_nama ?> -
-			<?= $data_level ?>
-		</h1>
-	</center>
-</div>
-<hr>
-<div class="col-md-3 col-sm-6 col-xs-6">
-	<div class="panel panel-back noti-box">
-		<span class="icon-box bg-color-red set-icon">
-			<i class="fa fa-bell"></i>
-		</span>
-		<div class="text-box">
-			<h1>
-				<?= $proses; ?>
-			</h1>
-			<p>Pengaduan Menunggu</p>
-
-		</div>
-	</div>
-</div>
-<div class="col-md-3 col-sm-6 col-xs-6">
-	<div class="panel panel-back noti-box">
-		<span class="icon-box bg-color-green set-icon">
-			<i class="fa fa-bell"></i>
-		</span>
-		<div class="text-box">
-			<h1>
-				<?= $tangan; ?>
-			</h1>
-			<p>Pengaduan Ditanggapi</p>
-
-		</div>
-	</div>
-</div>
-<div class="col-md-3 col-sm-6 col-xs-6">
-	<div class="panel panel-back noti-box">
-		<span class="icon-box bg-color-blue set-icon">
-			<i class="fa fa-bell"></i>
-		</span>
-		<div class="text-box">
-			<h1>
-				<?= $sel; ?>
-			</h1>
-			<p>Pengaduan Selesai</p>
-
-		</div>
-	</div>
-</div>
-<div class="col-md-3 col-sm-6 col-xs-6">
-	<div class="panel panel-back noti-box">
-		<span class="icon-box bg-color-custom set-icon">
-			<a class="fa fa-plus" href="?page=aduan_tambah" style="color:white;"></a>
-		</span>
-		<div class="text-box">
-			<h1>
-				+
-			</h1>
-			<p>
-				<a href="?page=aduan_tambah_form">Tambah Aduan</a>
-			</p>
-		</div>
-	</div>
-</div>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<!-- Copyrights -->
-<footer>
-	<div class="bg-light py-4">
-		<div class="container text-center">
-			<a href="https://www.instagram.com/stmijakarta/" target="blank">© 2025 - Sicepu</a>
-		</div>
-	</div>
-</footer>
-<!-- End -->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard Pelapor</title>
+    <link href="../assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="../assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="../assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <link href="../assets/css/custom.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="pengadu.php">Pelapor Dashboard</a> 
+            </div>
+            <div style="color: white; padding: 15px 50px 5px 50px; float: right; font-size: 16px;"> 
+                Last access : <?php echo date("d-M-Y H:i"); ?> &nbsp; 
+                <a href="../logout.php" class="btn btn-danger square-btn-adjust">Logout</a> 
+            </div>
+        </nav>   
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                    <li class="text-center">
+                        <img src="../assets/img/find_user.png" class="user-image img-responsive"/>
+                    </li>
+                    <li><a class="active-menu"  href="pengadu.php"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a></li>
+                    <li><a  href="../pengadu/aduan/adu_tambah_form.php"><i class="fa fa-edit fa-3x"></i> Buat Pengaduan</a></li>
+                    <li><a  href="../pengadu/aduan/adu_tampil.php"><i class="fa fa-eye fa-3x"></i> Lihat Pengaduan Saya</a></li>
+                    </ul>
+            </div>
+        </nav>  
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Pelapor Dashboard</h2>   
+                        <h5>Selamat datang, <?php echo htmlspecialchars($_SESSION['nama_lengkap']); ?> ! Anda login sebagai <?php echo htmlspecialchars($_SESSION['level']); ?>. </h5>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+                        <div class="panel panel-back noti-box">
+                            <span class="icon-box bg-color-blue set-icon">
+                                <i class="fa fa-file-text-o"></i>
+                            </span>
+                            <div class="text-box" >
+                                <p class="main-text">5 Pengaduan</p>
+                                <p class="text-muted">Total Anda</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+                        <div class="panel panel-back noti-box">
+                            <span class="icon-box bg-color-green set-icon">
+                                <i class="fa fa-check"></i>
+                            </span>
+                            <div class="text-box" >
+                                <p class="main-text">3 Pengaduan</p>
+                                <p class="text-muted">Selesai</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-6">           
+                        <div class="panel panel-back noti-box">
+                            <span class="icon-box bg-color-red set-icon">
+                                <i class="fa fa-clock-o"></i>
+                            </span>
+                            <div class="text-box" >
+                                <p class="main-text">2 Pengaduan</p>
+                                <p class="text-muted">Pending</p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                <hr />
+                </div>
+             </div>
+         </div>
+     <script src="../assets/js/jquery-1.10.2.js"></script>
+      <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/jquery.metisMenu.js"></script>
+     <script src="../assets/js/morris/raphael-2.1.0.min.js"></script>
+     <script src="../assets/js/morris/morris.js"></script>
+      <script src="../assets/js/custom.js"></script>
+    
+   
+</body>
+</html>
