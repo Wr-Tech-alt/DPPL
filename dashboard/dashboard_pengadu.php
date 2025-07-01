6ba352a5eee102ee82fca8a46d06e72dcd2fb20e
lordVoltech <124055040+lordVoltech@users.noreply.github.com>
2025-07-01 21:06:52 +0000

    redirecting riwayat

--- dashboard/dashboard_pengadu.php
+++ dashboard/dashboard_pengadu.php
@@ -244,7 +244,7 @@ $conn->close();
             <nav class="sidebar-nav">
                 <ul>
                     <li class="active"><a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
-                    <li><a href="#"><i class="fas fa-clipboard-list"></i> Riwayat Pengaduan</a></li>
+                    <li><a href="../pengadu/riwayat_aduan.php"><i class="fas fa-clipboard-list"></i> Riwayat Pengaduan</a></li>
                     <li><a href="#"><i class="fas fa-cog"></i> Pengaturan Akun</a></li>
                 </ul>
             </nav>
