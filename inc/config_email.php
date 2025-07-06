268d3779bb256ce1d68b980f2e6f04414cc5e20d
LordVoltech <syamilalghani.9i@gmail.com>
2025-07-06 12:22:22 +0700

    mailer lagi

--- inc/config_email.php
+++ inc/config_email.php
@@ -5,8 +5,8 @@
 // PENTING: Gunakan App Password jika pakai Gmail, bukan password akun Gmail kamu.
 // Cari tahu cara membuat App Password di pengaturan akun Google-mu.
 define('EMAIL_SENDER_USERNAME', 'ryuugavariantes@gmail.com'); // Ganti dengan email pengirim notifikasi
-define('EMAIL_SENDER_PASSWORD', 'B4n4gh3r333$#Ryuuga'); // Ganti dengan App Password Gmail
+define('EMAIL_SENDER_PASSWORD', 'mbyc vbyx kesy iacf'); // Ganti dengan App Password Gmail
 define('EMAIL_SENDER_NAME', 'SiCepu Kampus Pilihan Anda STMI :)'); // Nama pengirim yang akan muncul
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
-//define('SMTP_SECURE', PHPMailer::ENCRYPTION_STARTTLS); // Atau PHPMailer::ENCRYPTION_SMTPS untuk port 465
+//define('SMTP_SECURE', PHPMailer::ENCRYPTION_STARTTLS); // Atau PHPMailer::ENCRYPTION_SMTPS untuk port 46
